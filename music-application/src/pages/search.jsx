e1c02612f4c91d5ae1d06947d21f853b0abcd955
OlekOlaisen <olekristianolaisen@gmail.com>
2023-06-28 02:53:55 +0200

    Styled search input

--- music-application/src/pages/search.jsx
+++ music-application/src/pages/search.jsx
@@ -1,9 +1,15 @@
-function Home() {
+import { Search } from 'react-bootstrap-icons';
+
+function SearchBar() {
   return (
 	<section className="search">
-    <input type="text" name="" id="" />
+    <label className="search__label" htmlFor="search"></label>
+    <div className="search__input-container">
+      <input className="search__input" type="text" name="search" id="search" placeholder="What do you want to listen to?" />
+      <div className="search__input-icon"><Search /></div>
+    </div>
   </section>
   );
 }
 
-export default Home;
+export default SearchBar;
