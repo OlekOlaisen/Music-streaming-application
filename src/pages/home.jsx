05ced47d83d0166fe59e8100b5a5e029bee92c34
OlekOlaisen <olekristianolaisen@gmail.com>
2023-07-06 17:23:15 +0200

    added simple logo to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/images/logo.png'
+
 function Home() {
   return (
 	<main className="home">
-    <p>Home</p>
+    <img className='home__logo' src={logo} alt="logo" />
     <div className="home__trending-tracks"></div>
     <div className="home__trending-artists"></div>
   </main>
