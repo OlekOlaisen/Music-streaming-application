7286c691209cb5f578f61d7fa9565f492dd6a068
OlekOlaisen <olekristianolaisen@gmail.com>
2023-07-08 02:21:14 +0200

    Added some desktop desponsitivity and volume slide

--- src/components/audioContext.jsx
+++ src/components/audioContext.jsx
@@ -70,7 +70,8 @@ export const AudioProvider = ({ children }) => {
         setPlaylist,
         setCurrentIndex,
         currentIndex,
-        currentSong, 
+        currentSong,
+        audioRef 
       }}
     >
       {children}
--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useContext, useRef, useEffect } from 'react';
-import { Shuffle, SkipStartFill, PlayFill, PauseFill, SkipEndFill, Repeat, Repeat1, Heart, ChevronDown } from 'react-bootstrap-icons';
+import {
+  Shuffle,
+  SkipStartFill,
+  PlayFill,
+  PauseFill,
+  SkipEndFill,
+  Repeat,
+  Repeat1,
+  Heart,
+  ChevronDown,
+  VolumeMuteFill,
+  VolumeDownFill,
+  VolumeUpFill,
+} from 'react-bootstrap-icons';
 import { AudioContext } from './audioContext.jsx';
 
 function Player() {
-  const { isPlaying, togglePlay, playNext, playPrevious, currentSong } = useContext(AudioContext);
+  const { isPlaying, togglePlay, playNext, playPrevious, currentSong, audioRef } = useContext(AudioContext);
   const [repeatMode, setRepeatMode] = useState(0);
-  const audioRef = useRef(null);
   const titleRef = useRef(null);
   const [hasPlayedSong, setHasPlayedSong] = useState(false);
   const [controlsExpanded, setControlsExpanded] = useState(false);
+  const [volumeMuted, setVolumeMuted] = useState(false);
+  const [prevVolume, setPrevVolume] = useState(1);
+  const [currentVolume, setCurrentVolume] = useState(1);
 
   const handleError = (error) => {
     if (
@@ -19,6 +34,32 @@ function Player() {
     }
 
     console.error('Error playing audio:', error);
+  };
+
+  const changeVolume = (e) => {
+    const volumeValue = parseFloat(e.target.value);
+    audioRef.current.volume = volumeValue;
+
+    if (volumeValue === 0) {
+      setVolumeMuted(true);
+    } else {
+      setVolumeMuted(false);
+    }
+
+    setCurrentVolume(volumeValue);
+    setPrevVolume(volumeValue);
+  };
+
+  const toggleVolume = () => {
+    if (volumeMuted) {
+      audioRef.current.volume = prevVolume;
+      setCurrentVolume(prevVolume);
+      setVolumeMuted(false);
+    } else {
+      audioRef.current.volume = 0;
+      setCurrentVolume(0);
+      setVolumeMuted(true);
+    }
   };
 
   useEffect(() => {
@@ -32,7 +73,7 @@ function Player() {
         audio.removeEventListener('error', handleError);
       }
     };
-  }, []);
+  }, [audioRef]);
 
   useEffect(() => {
     if (isPlaying) {
@@ -41,7 +82,7 @@ function Player() {
     } else {
       audioRef.current.pause();
     }
-  }, [isPlaying]);
+  }, [isPlaying, audioRef]);
 
   useEffect(() => {
     const titleElement = titleRef.current;
@@ -92,99 +133,148 @@ function Player() {
     }
   }, [controlsExpanded]);
 
-      return (
-      <section className='player'>
-      <div className={`player ${hasPlayedSong ? 'show' : ''}`}>
-      <div className={`player__container ${albumImage ? 'hasBackground' : ''}`}
-      style={albumImage ? { '--bg-image': `url(${albumImage})` } : {}} >
-      <div className="player__info" onClick={toggleControlsExpanded}>
-      <div className='player__info-cover'>
-      {albumImage && (
-        <img src={albumImage} alt="Album" className="player__info-album-image" />
-        )}
-        </div>
-        <div className='player__info-details'>
-        {songTitle && (
-          <p className="player__info-song-title" ref={titleRef}>{songTitle}</p>
-          )}
-          {artistName && (
-            <p className="player__info-artist-name">{artistName}</p>
-            )}
+  return (
+    <section className='player'>
+      <div className={`player ${hasPlayedSong ? 'show' : ''} `}>
+        <div
+          className={`player__container ${albumImage ? 'hasBackground' : ''}`}
+          style={albumImage ? { '--bg-image': `url(${albumImage})` } : {}}
+        >
+          <div className="player__info" onClick={toggleControlsExpanded}>
+            <div className='player__info-cover'>
+              {albumImage && (
+                <img src={albumImage} alt="Album" className="player__info-album-image" />
+              )}
             </div>
+            <div className='player__info-details'>
+              {songTitle && (
+                <p className="player__info-song-title" ref={titleRef}>{songTitle}</p>
+              )}
+              {artistName && (
+                <p className="player__info-artist-name">{artistName}</p>
+              )}
             </div>
-            <div className="player__controls">
+          </div>
+          <div className="player__controls">
             {hasPlayedSong && (
               <div className="player__buttons">
-              <button className="player__controls-toggle button" onClick={togglePlay}>
+                <button className="player__controls-toggle button" onClick={togglePlay}>
+                  {isPlaying ? (
+                    <PauseFill className="player__controls-toggle-pause" />
+                  ) : (
+                    <PlayFill className="player__controls-toggle-play" />
+                  )}
+                </button>
+                <button className="player__button">
+                  <Heart className="player__button-icon" />
+                </button>
+              </div>
+            )}
+          </div>
+
+          <div className="player__controls-buttons-desktop">
+            <button className="player__controls-shuffle button">
+              <Shuffle />
+            </button>
+            <button className="player__controls-rewind button" onClick={playPrevious}>
+              <SkipStartFill />
+            </button>
+            <button className="player__controls-toggle button" onClick={togglePlay}>
               {isPlaying ? (
                 <PauseFill className="player__controls-toggle-pause" />
+              ) : (
+                <PlayFill className="player__controls-toggle-play" />
+              )}
+            </button>
+            <button className="player__controls-forward button" onClick={playNext}>
+              <SkipEndFill />
+            </button>
+            <button className="player__controls-loop button" onClick={toggleRepeat}>
+              {repeatMode === 0 && <Repeat className="player__controls-loop-off" />}
+              {repeatMode === 1 && <Repeat className="player__controls-loop-on" />}
+              {repeatMode === 2 && <Repeat1 className="player__controls-loop-1" />}
+            </button>
+          </div>
+
+          <div className="player__audio">
+            {volumeMuted ? (
+              <button onClick={toggleVolume}>
+                <VolumeMuteFill className="player__audio-mute button" />
+              </button>
+            ) : (
+              <button onClick={toggleVolume}>
+                <VolumeUpFill className="player__audio-up button" />
+              </button>
+            )}
+            <input
+              type="range"
+              id="volume"
+              className='player__audio-slider'
+              name="vol"
+              min="0"
+              max="1"
+              step="0.01"
+              value={currentVolume}
+              onChange={changeVolume}
+            />
+          </div>
+
+        </div>
+
+        <div classNameclassName="player__progress"><p></p></div>
+
+        <audio ref={audioRef} />
+      </div>
+
+      {controlsExpanded && (
+        <section
+          className={`player__controls-expanded ${controlsExpanded ? 'open' : ''} ${
+            albumImage ? 'hasBackground' : ''
+          }`}
+          style={albumImage ? { '--bg-image': `url(${albumImage})` } : {}}
+        >
+          <button className="player__controls-close button" onClick={toggleControlsExpanded}>
+            <ChevronDown className="player__controls-close-icon" />
+          </button>
+
+          <div className='player__controls-container'>
+            <div className="player__controls-song-details">
+              <img src={albumImage} alt="Album" className="player__controls-album-image" />
+              <div className="player__controls-song-info">
+                <p className="player__controls-song-title">{songTitle}</p>
+                <p className="player__controls-artist-name">{artistName}</p>
+              </div>
+            </div>
+
+            <div className="player__controls-buttons">
+              <button className="player__controls-shuffle button">
+                <Shuffle />
+              </button>
+              <button className="player__controls-rewind button" onClick={playPrevious}>
+                <SkipStartFill />
+              </button>
+              <button className="player__controls-toggle button" onClick={togglePlay}>
+                {isPlaying ? (
+                  <PauseFill className="player__controls-toggle-pause" />
                 ) : (
                   <PlayFill className="player__controls-toggle-play" />
-                  )}
-                  </button>
-                  <button className="player__button">
-                  <Heart className="player__button-icon" />
-                  </button>
-                  </div>
-                  )}
-                  </div>
-                  </div>           
-                  <div className="player__progress">
-                  </div>
-                  
-                  <audio ref={audioRef} />
-                  </div>
-                  {controlsExpanded && (
-                    <section
-                    className={`player__controls-expanded ${controlsExpanded ? 'open' : ''} ${
-                      albumImage ? 'hasBackground' : ''
-                    }`}
-                    style={albumImage ? { '--bg-image': `url(${albumImage})` } : {}}
-                    >
-                    <button className="player__controls-close button" onClick={toggleControlsExpanded}>
-                    <ChevronDown className="player__controls-close-icon" />
-                    </button>
-                    
-                    <div className='player__controls-container'>
-                    <div className="player__controls-song-details">
-                    <img src={albumImage} alt="Album" className="player__controls-album-image" />
-                    <div className="player__controls-song-info">
-                    <p className="player__controls-song-title">{songTitle}</p>
-                    <p className="player__controls-artist-name">{artistName}</p>
-                    
-                    </div>
-                    </div>
-                    
-                    <div className="player__controls-buttons">
-                    <button className="player__controls-shuffle button">
-                    <Shuffle />
-                    </button>
-                    <button className="player__controls-rewind button" onClick={playPrevious}>
-                    <SkipStartFill />
-                    </button>
-                    <button className="player__controls-toggle button" onClick={togglePlay}>
-                    {isPlaying ? (
-                      <PauseFill className="player__controls-toggle-pause" />
-                      ) : (
-                        <PlayFill className="player__controls-toggle-play" />
-                        )}
-                        </button>
-                        <button className="player__controls-forward button" onClick={playNext}>
-                        <SkipEndFill />
-                        </button>
-                        <button className="player__controls-loop button" onClick={toggleRepeat}>
-                        {repeatMode === 0 && <Repeat className="player__controls-loop-off" />}
-                        {repeatMode === 1 && <Repeat className="player__controls-loop-on" />}
-                        {repeatMode === 2 && <Repeat1 className="player__controls-loop-1" />}
-                        </button>
-                        </div>
-                        
-                        </div>
-                        </section>
-                        )}
-                        </section>
-                        );
-                      }
-                      
-                      export default Player;
-                      
+                )}
+              </button>
+              <button className="player__controls-forward button" onClick={playNext}>
+                <SkipEndFill />
+              </button>
+              <button className="player__controls-loop button" onClick={toggleRepeat}>
+                {repeatMode === 0 && <Repeat className="player__controls-loop-off" />}
+                {repeatMode === 1 && <Repeat className="player__controls-loop-on" />}
+                {repeatMode === 2 && <Repeat1 className="player__controls-loop-1" />}
+              </button>
+            </div>
+          </div>
+        </section>
+      )}
+
+    </section>
+  );
+}
+
+export default Player;
